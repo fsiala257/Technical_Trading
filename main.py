26e2e6d94948c03e4cae4427e32b0546ba80729f
fsiala257 <firas.siala257@gmail.com>
2022-01-29 13:08:26 -0500

    Initial commit

--- main.py
+++ main.py
@@ -5,40 +5,61 @@ import yfinance as yf
 plt.style.use('fivethirtyeight')
 
 
-# SIMPLE MOVING AVERAGE
-def SMA(data,period=30,column='close'):
+class tradingBot:
+
+    def readFile(self,fileName):
+        df = pd.read_csv(fileName)
+        return df;
+
+    def SMA(self,data,period,column='Close'):
         return data[column].rolling(window=period).mean()
 
-# EXPONENTIONAL MOVING AVERAGE
-def EMA(data,period=20,column='close'):
-    return data[column].ewm(span=period,adjust=False).mean()
+    def EMA(self,data,period,column='Close'):
+        return data[column].ewm(span=period,adjust=False).mean()
 
-# MOVING AVERAGE CONVERGENCE/DIVERGENCE
-def MACD(data,period_long=26,period_short=12,period_signal=9, column='Close'):
-    # SHORT TERM EMA
-    shortEMA = EMA(data,period_short,column=column)
-    # LONG TERM EMA
-    longEMA = EMA(data,period_long,column=column)
-    # MACD
-    data['MACD'] = shortEMA - longEMA 
-    # SIGNAL
-    data['Signal_Line'] = EMA(data,period_signal,column='MACD')
+    
 
-    return data
 
-# RELATIVE STRENGTH INDICATOR
-def RSI(data,period=14,column='Close'):
-    delta = data[column].diff(1) 
-    delta = delta[1:]
-    up = delta.copy()
-    down = delta.copy()
-    up[up<0] = 0
-    down[down>0] = 0
-    data['up'] = up
-    data['down'] = down
-    AVG_Gain = SMA(data,period,column='up')
-    AVG_Loss = abs(SMA(data,period,column='down'))
-    RS = AVG_Gain/AVG_Loss
-    RSI = 100.0-(100.0/(1.0+RS))
-    data['RSI'] = RSI
-    return data
+
+bot1 = tradingBot()
+df = bot1.readFile('BTCUSDT.csv')
+df2 = bot1.SMA(df,120)
+df3 = bot1.EMA(df,120)
+
+
+plt.plot(df['Close'])
+plt.plot(df3)
+plt.show()
+
+
+
+
+# # MOVING AVERAGE CONVERGENCE/DIVERGENCE
+# def MACD(data,period_long=26,period_short=12,period_signal=9, column='Close'):
+#     # SHORT TERM EMA
+#     shortEMA = EMA(data,period_short,column=column)
+#     # LONG TERM EMA
+#     longEMA = EMA(data,period_long,column=column)
+#     # MACD
+#     data['MACD'] = shortEMA - longEMA 
+#     # SIGNAL
+#     data['Signal_Line'] = EMA(data,period_signal,column='MACD')
+
+#     return data
+
+# # RELATIVE STRENGTH INDICATOR
+# def RSI(data,period=14,column='Close'):
+#     delta = data[column].diff(1) 
+#     delta = delta[1:]
+#     up = delta.copy()
+#     down = delta.copy()
+#     up[up<0] = 0
+#     down[down>0] = 0
+#     data['up'] = up
+#     data['down'] = down
+#     AVG_Gain = SMA(data,period,column='up')
+#     AVG_Loss = abs(SMA(data,period,column='down'))
+#     RS = AVG_Gain/AVG_Loss
+#     RSI = 100.0-(100.0/(1.0+RS))
+#     data['RSI'] = RSI
+#     return data
